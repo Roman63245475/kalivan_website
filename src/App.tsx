15c8001fe126cd954f918c8235975ccecda6e624
Roman <yummison44@gmail.com>
2026-08-16 20:01:19 +0200

    deletion of posts now works

--- src/App.tsx
+++ src/App.tsx
@@ -20,27 +20,41 @@ export function App(search: boolean = false, input: string = '') {
         fetch('https://dummyjson.com/posts').then(res => res.json()).then(json => setPosts(json.posts));
     },[])
 
+    function deletePost(postId: number){
+        setPosts(posts.filter((post) => post.id !== postId));
+    }
+
   return (
       <div className={'whole_page'}>
           <SearchComponent />
           <div className={'feed'}>
-              {getFeed(posts)}
+              {posts.map(post => (
+                  <GetSinglePost key={post.id} post={post} onDelete={deletePost}/>
+              ))}
           </div>
       </div>
   );
 }
 
-function getFeed(posts: Post[]){
-    return posts.map(post => GetSinglePost(post));
-}
 
-function GetSinglePost(post: Post) {
+
+function GetSinglePost(props: {post: Post, onDelete: (postId: number) => void}) {
+    const post = props.post;
     return (
-        <div key={post.id}>
+        <div>
             <h1>{post.title}</h1>
             <h2>{post.body}</h2>
             {GetPrettyTags(post.tags)}
             <h3>Likes: {post.reactions.likes} | Dislikes: {post.reactions.dislikes} | Views: {post.views}</h3>
+            <div className={'actions'}>
+                <button className={'delete_post'} onClick={() => props.onDelete(post.id)}>
+                    Delete Post
+                </button>
+
+                <button className={'view_post'}>
+                    View Post
+                </button>
+            </div>
         </div>
     )
 }
